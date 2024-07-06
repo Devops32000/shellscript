17bbf764e308994134cdf8d217554be1298c5f48
Rajesh <rajesh23@gmail>
2024-07-06 14:54:09 +0530

    ok

--- 01.sh
+++ 01.sh
@@ -1,3 +1,9 @@
 #!/bin/bash 
 
-echo "Hello, I am learning devops"
+# echo "Hello, I am learning devops"
+
+movies=("rajesh" "keerthi" "eshwar")
+
+echo "this is ${movies[0]}"
+echo "this is ${movies[1]}"
+echo "this is ${movies[2]}"
