c0ee5febc4fdf38cd7c067abfbcecac2aa101854
Rajesh <rajesh23@gmail>
2024-07-07 11:11:39 +0530

    ok

--- 01.sh
+++ 01.sh
@@ -1,12 +1,32 @@
 #!/bin/bash 
 
-num=$1
+userid=$( id -u )
+timestamp=$( date +%F-%H-%M-%S)
+scriptname=$( $0 | cut -d "." -f1 )
+logfile=/tmp/$timestamp-$scriptname.log 
 
-if [ $num -gt 10 ]
+R="\e[31m"
+G="\e[32m"
+N="\e[0M"
+
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R Failed $N"
+        exit 1
+    else 
+        echo -e "$2....$G Success $N"
+    fi 
+}
+
+if [ userid -ne 0 ]
 then 
-    echo "Given $num is greater than 10"
+    echo "Please run with sudo previliges" 
+    exit 1 
 else
-    echo "Given $num is less than 10"
+    echo "Yeahhh you are a super user"
 fi 
 
+dnf install mysql -y &>>logfile
+validate $? "Installing mysql" 
 
