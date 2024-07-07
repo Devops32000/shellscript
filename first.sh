3a5cc1160aa291fcd3d9eb84679725a5e1cf3b43
Rajesh <rajesh23@gmail>
2024-07-07 11:32:47 +0530

    ok

--- first.sh
+++ first.sh
@@ -1,30 +1,30 @@
-#!/bin/bash
-
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+userid=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+scriptname=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$timestamp-$scriptname.log 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo "$2...FAILURE"
-        exit 1
-    else
-        echo "$2...SUCCESS"
-    fi
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2....$R FAILURE $N"
+    else 
+        echo -e "$2....$G SUCCESS $N"
+    fi 
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+if [ $userid -ne 0 ]
+then 
+    echo "Please run with sudo previliges"
+    exit 1
 else
-    echo "You are super user."
-fi
+    echo "You are a super user"
+fi 
 
-dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL"
+dnf install mysql -y &>>$logfile
+VALIDATE $? "installing mysql" 
 
-dnf install git -y &>>$LOGFILE
-VALIDATE $? "Installing Git"
+dnf remove mysql -y &>>$logfile
+VALIDATE $? "Termintaing mysql" 
