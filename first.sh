62e08cee25e5a087f37c06da067e5f377379c014
Rajesh <rajesh23@gmail>
2024-07-07 12:15:54 +0530

    ok

--- first.sh
+++ first.sh
@@ -1,30 +1,43 @@
-userid=$(id -u)
-timestamp=$(date +%F-%H-%M-%S)
-scriptname=$(echo $0 | cut -d "." -f1)
-logfile=/tmp/$timestamp-$scriptname.log 
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2....$R FAILURE $N"
-    else 
-        echo -e "$2....$G SUCCESS $N"
-    fi 
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
 
-if [ $userid -ne 0 ]
-then 
-    echo "Please run with sudo previliges"
-    exit 1
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "You are a super user"
-fi 
+    echo "You are super user."
+fi
 
-dnf install mysql -y &>>$logfile
-VALIDATE $? "installing mysql" 
 
-dnf remove mysql -y &>>$logfile
-VALIDATE $? "Termintaing mysql" 
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting up root password"
