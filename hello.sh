628d5c2e98c352d96ee34d4c5e10195b61c669f7
Rajesh <rajesh23@gmail>
2024-08-27 12:19:35 +0530

    some-message

--- hello.sh
+++ hello.sh
@@ -1,2 +1,12 @@
-echo 'Hi this is rajesh'
-echo "All variables: $@"
+#!/bin/bash
+
+#declaring of varible
+PERSON1=$1
+PERSON2=$2
+
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are youu?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
+echo "Number of variables passed: $#"
